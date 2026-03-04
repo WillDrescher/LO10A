ef1cec17ea942ca41aeed7dce0200b251acf9182
Will Drescher <will_drescher@mines.edu>
2026-03-03 18:57:08 -0700

    cat with whiskers

--- Cat.java
+++ Cat.java
@@ -19,6 +19,10 @@ public class Cat {
 	// mouth starts about 40% from left edge of head
 	private static final int MOUTH_X = HEAD_DIMENSION/5 * 2;
 	private static final int MOUTH_Y = HEAD_DIMENSION/5 * 3;
+	// whiskers are about 1/4 the size of the head
+	private static final int WHISKER_LENGTH = HEAD_DIMENSION/4;
+	private static final int WHISKER_SLANT = 5;
+	private static final int NUM_WHISKERS = 3;
 	
 	// draw will render the Cat on the Graphics object
 	public void draw(Graphics g, int catX, int catY)
@@ -29,6 +33,33 @@ public class Cat {
 		// Draw the head
 		g2.setColor(Color.black);
 		g2.fillOval(x, y, HEAD_DIMENSION, HEAD_DIMENSION);
+		;
+		
+		// Draw whiskers
+		g2.setColor(Color.white);
+		// Draw whiskers on the left of face
+		// -2 draws whisker just to left of mouth
+		x = catX + MOUTH_X - 2;
+		// x2 is the other endpoint, depends on length of whisker 
+		int x2 = x - WHISKER_LENGTH;
+		// whiskers all start at the same "height" as the mouth, centered
+		y = catY + MOUTH_Y + MOUTH_HEIGHT/2;
+		// -5 gives a slight slant
+		int y2 = y - WHISKER_SLANT;
+		for (int i=0; i<NUM_WHISKERS; i++)
+		{
+			g2.drawLine(x, y, x2, y2);
+			y2 += WHISKER_SLANT; 			
+		}
+		// Draw whiskers on the right of face, y stays the same
+		x = catX + MOUTH_X + MOUTH_WIDTH + 2;
+		x2 = x + WHISKER_LENGTH; 
+		y2 = y - WHISKER_SLANT;
+		for (int i=0; i<NUM_WHISKERS; i++)
+		{
+			g2.drawLine(x, y, x2, y2);
+			y2 += WHISKER_SLANT; 			
+		}
 		// Draw the eyes
 		g2.setColor(Color.green);
 		x = catX + EYE_X; 
